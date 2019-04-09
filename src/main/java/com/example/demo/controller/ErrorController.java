d4969df3c2c8fa048c80611c174b32110fe48c38
youngho <jason.uphiller@gmail.com>
2019-04-09 10:02:07 +0900

    add error setting

--- src/main/java/com/example/demo/controller/ErrorController.java
+++ src/main/java/com/example/demo/controller/ErrorController.java
@@ -2,10 +2,11 @@ package com.example.demo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-//@ControllerAdvice
+@ControllerAdvice
 public class ErrorController {
 
     @ExceptionHandler(Throwable.class)
